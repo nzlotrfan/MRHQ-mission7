a7c61a8045e18a33eee934173a9ababb9e831cd5
Reuben Simpson <useyourtimewell@gmail.com>
2022-04-06 23:44:47 +1200

    fixed spelling errors

--- m7frontend/src/components/Header.js
+++ m7frontend/src/components/Header.js
@@ -3,23 +3,43 @@ import "./Header.css";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const handleBtnClick = function () {
+    alert('Nice try! Only the "Property Search" button works.');
+  };
+
   return (
     <div className="header-container">
       <div className="header-top">
         <img src={logo} className="header-logo" />
         <p>
-          Login or <button className="header-btn header-login-btn">Sign up</button>
+          Login or{" "}
+          <button
+            onClick={handleBtnClick}
+            className="header-btn header-login-btn"
+          >
+            Sign up
+          </button>
         </p>
       </div>
       <div className="header-navbar">
-        <button className="header-btn header-nav-btn">Property Management</button>
+        <button onClick={handleBtnClick} className="header-btn header-nav-btn">
+          Property Management
+        </button>
         <NavLink to="/search" className="header-btn header-nav-btn">
           Property Search
         </NavLink>
-        <button className="header-btn header-nav-btn">Body Corporate</button>
-        <button className="header-btn header-nav-btn">Commercial</button>
-        <button className="header-btn header-nav-btn">Waiheke Escapes</button>
-        <button className="header-btn header-nav-btn">FAQ &amp; Contacts</button>
+        <button onClick={handleBtnClick} className="header-btn header-nav-btn">
+          Body Corporate
+        </button>
+        <button onClick={handleBtnClick} className="header-btn header-nav-btn">
+          Commercial
+        </button>
+        <button onClick={handleBtnClick} className="header-btn header-nav-btn">
+          Waiheke Escapes
+        </button>
+        <button onClick={handleBtnClick} className="header-btn header-nav-btn">
+          FAQ &amp; Contacts
+        </button>
       </div>
     </div>
   );
